4801ffbecd417d1caf2486da43fc68ff384d161c
Edson Rocha <rocha.edsonn@gmail.com>
2025-03-27 11:16:32 -0300

    atualizado

--- app.py
+++ app.py
@@ -9,17 +9,25 @@ def index():
 
 @app.route('/contador')
 def contador():
+    # Data inicial de 26 de março de 2025
+    data_inicial = datetime(2025, 3, 26)
+    
+    # Data final de 17 de abril de 2025
+    data_final = datetime(2025, 4, 17)
+    
     # Obter a data atual automaticamente
     data_atual = datetime.now()
     
-    # Definir a data final (23 de abril de 2025)
-    data_final = datetime(2025, 4, 17)
-    
-    # Calcular os dias faltantes
+    # Calcular os dias faltantes entre a data inicial e a data final
     dias_faltantes = (data_final - data_atual).days
 
-    # Retornar o número de dias faltantes
-    return jsonify({'mensagem': f'Faltam apenas {dias_faltantes} dias para o fim do aviso.'})
+    if data_atual > data_final:
+        mensagem = "Chegou ao fim seu ciclo."
+    else:
+        mensagem = f'Faltam apenas {dias_faltantes} dias para o fim do aviso.'
+    
+    # Retornar a mensagem
+    return jsonify({'mensagem': mensagem})
 
 if __name__ == '__main__':
     app.run(debug=True)
